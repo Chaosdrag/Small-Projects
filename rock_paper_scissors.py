eb4a4b3a4edde15f9255b623fb075b7bd317e7dc
red <choasdrag2@gmail.com>
2025-03-21 19:56:10 +0800

    improve code structure and clean up code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,30 +1,53 @@
 import random
 
-if __name__ == '__main__':
+ROCK = 'r'
+PAPER = 'p'
+SCISSORS = 's'
 
-    options = ('r', 'p', 's')
-    emojis = {'r': '🪨', 'p': '📜', 's': '✂️'}
-while True:
-    user_choice = input('Rock, Paper, or Scissors? (r/p/s):').lower()
-    if user_choice not in options:
-        print('Invalid input')
-        continue
+emojis = {ROCK: '🪨', PAPER: '📜', SCISSORS: '✂️'}
+options = tuple(emojis.keys())
 
-    comp_choice = random.choice(options)
 
+def player_choice():
+    while True:
+        user_choice = input('Rock, Paper, or Scissors? (r/p/s):').lower()
+        if user_choice in options:
+            return user_choice
+        else:
+            print('Invalid input')
+
+
+def display_choices(user_choice, comp_choice):
     print(f'You chose {emojis[user_choice]}')
     print(f'Computer chose {emojis[comp_choice]}')
 
+
+def determine_winner(user_choice, comp_choice):
     if user_choice == comp_choice:
         print('It\'s a tie!')
     elif (
-        (user_choice == 'r' and comp_choice == 's')
-        or (user_choice == 'p' and comp_choice == 'r')
-            or (user_choice == 's' and comp_choice == 'p')):
+        (user_choice == ROCK and comp_choice == SCISSORS)
+        or (user_choice == PAPER and comp_choice == ROCK)
+            or (user_choice == SCISSORS and comp_choice == PAPER)):
         print('You win!')
     else:
         print('You lose!')
 
-    cont = input('Do you want to play again? (y/n): ')
-    if cont == 'n':
-        break
+
+def play_game():
+    while True:
+
+        user_choice = player_choice()
+
+        comp_choice = random.choice(options)
+
+        display_choices(user_choice, comp_choice)
+
+        determine_winner(user_choice, comp_choice)
+
+        cont = input('Do you want to play again? (y/n): ')
+        if cont == 'n':
+            break
+
+
+play_game()
